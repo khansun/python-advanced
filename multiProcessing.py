99643a37d8a2dd5e17b939226fa582212e1cfdc7
Mahamudur Rahaman Khan <khan.mahmud@outlook.com>
2021-07-10 00:02:02 +0600

    execution time

--- multiProcessing.py
+++ multiProcessing.py
@@ -1,29 +1,32 @@
 from concurrent.futures import ProcessPoolExecutor, wait
-import codecs
+from tqdm import tqdm
 import re
 import os
 from pydub import AudioSegment
+from timeit import timeit
+
 def resample_audio(file):
-    print("Start: "+file)
-    newAudio = AudioSegment.from_wav(file)
-    newAudio = AudioSegment.from_wav(file)
+    newAudio = AudioSegment.from_wav(file)    
     newAudio = newAudio.set_frame_rate(22050)
     newAudio = newAudio.set_sample_width(2) 
     newAudio = newAudio.set_channels(1)
-    newAudio.export(re.sub(".wav", "_res.wav", file),format='wav')
-    print("End: "+file)
-
+    output = re.sub("wavs/", "wavs_res/", file)
+    newAudio.export(output,format='wav')
+    
+    
 
 def batch_resample(files):
     tasks = []
-
-    with ProcessPoolExecutor(max_workers=4) as executor:
-        for file in files:
+    
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        for file in tqdm(files):
             tasks.append(executor.submit(resample_audio, "wavs/"+file))
-
+        print("Batch Resampling Audio...")    
+    
     wait(tasks)
 
 
 if __name__ == "__main__":
-    filePaths = os.listdir("wavs")
-    batch_resample(filePaths)
+    execution_time = timeit('import os \nbatch_resample(os.listdir("wavs"))', number=1,setup="from __main__ import batch_resample")
+    print(str(len(os.listdir("wavs")))+" WAVs Resampled Successfully!")
+    print('Execution Time (s): ' + str(execution_time))
